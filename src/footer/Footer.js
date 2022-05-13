62accf9ed0a39c38c86e82f532874347aa4ddbcb
Barbos20 <92588487+Barbos20@users.noreply.github.com>
2022-05-13 16:58:04 +0300

    any

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,40 +1,10 @@
-// import React from 'react';
-// import style from './Footer.module.scss';
-// import styleContainer from '../common/styles/Container.module.css';
-// import Sponsors from '../footer/sponsors/Sponsors';
-// import TG from '../assect/img/icons/telegram.svg'
-
-// function Footer() {
-//     const telega = {
-//         backgroundImage: `url(${TG})`
-//       }
-    
-
-//   return (
-//     <div className={style.footerContainer}>
-//       <div className={`${styleContainer.container} ${style.footer}`}>
-//         <div className={style.by}>PROJECT BY KOROL ARTHUR</div>
-//         <div className={style.partImg}>
-//           <Sponsors style={telega} />
-//           <Sponsors title={'im sponsor'} />
-//           <Sponsors title={'im sponsor'} />
-//           <Sponsors title={'im sponsor'} />
-//         </div>
-//         <div className={style.info}>© 2021 ALL RIGHTS RESERVED</div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Footer;
-
-
-import React from 'react';
+  import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
-import Title from '../components/title/Title.js'
-import cssLogo from '../assect/img/icons/css-3.svg';
 import TG from '../assect/img/icons/telegram.svg';
 import Contact from './contact/Contact';
+import gitHub from '../assect/img/icons/github_logo.svg';
+import link from '../assect/img/icons/linkedin.svg';
 
 
 function Footer() {
@@ -47,10 +17,13 @@ function Footer() {
         icon: iconStyle(TG)
       },
       {
-        icon: iconStyle(TG)
+        icon: iconStyle(gitHub)
       },
       {
-        icon: iconStyle(TG)
+        icon: iconStyle(link)
+      },
+      {
+        icon: iconStyle(link)
       },
     ]
     return (
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import style from './Main.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
+
+// import styleContainer from '../common/styles/Container.module.css'
+
 
 
 function Main() {
-  return (
-    <div className={style.mainBlock}>
-      <div className={styleContainer.container}>
-        <div className={style.text} >
-        <h1><span> Hi there</span>
-        <br/>
-          I am Korol Arthur</h1>
-          <h2>Front-end Developer</h2>
+    return (
+
+        // <div className={style.mainBlock}>
+        //   <div className={style.container}>
+        //     <div className={style.greeting} >
+        //     <h1><span> Hi there</span>
+        //     <br/>
+        //       I am Korol Arthur</h1>
+        //       <h2>Front-end Developer</h2>
+        //     </div>
+        //     <div className={style.photo}>
+        //       <div className={style.image}/>
+        //     </div>
+        //   </div>
+        // </div>
+
+
+
+        <div className={style.mainBlock}>
+
+            <div className={style.container}>
+                {/* <div className={style.animated-back}></div> */}
+                <div >
+                <h1>Welcome There!
+                    <br />I'm Korol Arthur</h1>
+                <p>I'm a front-end developer with experience 
+                <br />building SPA's with Reack/Redux/Typescript. 
+                <br />In my spare time, I work on improving 
+                <br />the skills acquired during training.</p>
+                </div>
+                <div className={style.photo}>
+                    <div className={style.image} />
+                </div>
+
+
+
+            </div>
+
         </div>
-        <div className={style.photo}>
-          <div className={style.image}></div>
-        </div>
-      </div>
-    </div>
-  );
+
+    );
 }
+
+
+
 export default Main;
